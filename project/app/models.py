cfa2eaad038e450b804b19383e518857022a99f5
roppert <roppert@gmail.com>
2016-11-03 14:38:51 +0100

    Use image field .url shorthand in template. Make use of upload_to on image field.

--- project/app/models.py
+++ project/app/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Image(models.Model):
     name = models.CharField(max_length=128)
     description = models.TextField(blank=True)
-    image = models.ImageField()
+    image = models.ImageField(upload_to="images/")
 
     def __str__(self):
         return str(self.image)
